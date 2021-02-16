9a4515a3d5566304d6defad6660b1c40d01ca2a2
biiskot <gabriel.contois@gmail.com>
2021-02-16 15:00:10 +0100

    ajout fonction generation nom ville, remplissage du vecteur Listvilles

--- tp4 cpp s2/tp4 cpp s2/Source.cpp
+++ tp4 cpp s2/tp4 cpp s2/Source.cpp
@@ -20,47 +20,72 @@ void remplirVec(Ville& ville, std::vector <Ville>& tab) {
 	tab.push_back(ville);
 }
 
-void afficheVec(std::vector<std::string> v, int len) {
-	for (int i = 0; i < len; i++) {
+void afficheVec(std::vector<std::string> v) {
+	for (int i = 0; i < v.size(); i++) {
 		std::cout << v[i] << " ";
 	}
 	std::cout << std::endl;
 }
 
+//marche que pour 2 villes
 void Permutations(std::vector<std::string> &villes, int debut, int fin) {
 	if (debut == fin) {
-		afficheVec(villes,villes.size());
+		afficheVec(villes);
 	}
 	else {
 		for (int i = 0; i <= fin; i++) {
 			std::swap(villes[debut], villes[i]);
 			Permutations(villes, debut + 1, fin);
 			std::swap(villes[debut], villes[i]);
-			
 		}
 	}
 
 }
 
-void genNomsVilles(std::vector<std::string> v) {
+// A : 65
+// a : 97
+
+// int to ascii char
+char convert(int nombre)
+{
+	return nombre + 'a' - 1;
+}
+
+char convertmaj(int nombre)
+{
+	return nombre + 'A' - 1;
+}
+
+void genNomsVilles(std::vector<std::string> &v) {
 	int nbLettresNomVille = 0;
-	std::string nomTmp;
 	int N_to_C=0;
-	std::string c;
-	char num_char;
+	char C;
+	std::string nom;
 	for (int i = 0; i < nombreDeVilles; i++) {
-		nbLettresNomVille = tailleMinNomVille + rand()​ % ​(tailleMaxNomVille​ - ​tailleMinNomVille​ + ​1​);
+
+		nbLettresNomVille = tailleMinNomVille + rand() % (tailleMaxNomVille - tailleMinNomVille + 1);
+
 		for (int j = 0; j < nbLettresNomVille; j++) {
-			N_to_C = rand();
-			c = std::to_string(N_to_C);
-			num_char = c.c_str(N_to_C);
-			nomTmp[j] = num_char;
+			if (j == 0) {
+				N_to_C = rand() % 25 + 1;
+				nom.push_back(convertmaj(N_to_C));
+			}
+			else {
+				N_to_C = rand() % 25 + 1;
+				nom.push_back(convert(N_to_C));
+			}
 		}
-		v.push_back(nomTmp);
+		std::cout << nom << std::endl;
+		std::cout<<std::endl;
+		v.push_back(nom);
+		nom.resize(0);
 	}
+	std::cout << "vecteur des villes generees :" << std::endl;
+	afficheVec(v);
 }
 
 int main() {
-	std::srand(grainePourLeRands);
+	std::srand(time(nullptr));
 	Permutations(Listvilles, 0, Listvilles.size() - 1);
+	genNomsVilles(Listvilles);
 }
